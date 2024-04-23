109469685008079bf5d4d12aab5a5c5238b94e38
lijinzhu.azl <lijinzhu.azl@bytedance.com>
2024-04-23 17:33:29 +0800

    fix(config): fix ipam mode routing cidr judge

--- pkg/option/config.go
+++ pkg/option/config.go
@@ -3656,7 +3656,7 @@ func (c *DaemonConfig) checkMapSizeLimits() error {
 
 func (c *DaemonConfig) checkIPv4NativeRoutingCIDR() error {
 	if c.GetIPv4NativeRoutingCIDR() == nil && c.EnableIPv4Masquerade && !c.TunnelingEnabled() &&
-		c.IPAMMode() != ipamOption.IPAMENI && c.EnableIPv4 && c.IPAMMode() != ipamOption.IPAMAlibabaCloud || c.IPAMMode() == ipamOption.IPAMVolcengine {
+		c.IPAMMode() != ipamOption.IPAMENI && c.EnableIPv4 && c.IPAMMode() != ipamOption.IPAMAlibabaCloud || c.IPAMMode() != ipamOption.IPAMVolcengine {
 		return fmt.Errorf(
 			"native routing cidr must be configured with option --%s "+
 				"in combination with --%s --%s=%s --%s=%s --%s=true",
